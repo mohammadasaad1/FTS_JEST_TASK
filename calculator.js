e222d0d87f2d943e4bba3318e26a6917fa7ba4fd
mohammadasaad1 <mohammadfoqha2@hotmail.com>
2025-05-22 01:18:02 +0300

    TEST : IMPLEMENT ADDTION IN OUR CALC FUNCTION

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
-const calc = () => {};
+const calc = (num1, operator, num2) => {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+        throw new Error('Invalid input...'); 
+    }
+    switch(operator){
+        case '+' : return num1 + num2 ;
+        break;
+        default: throw new Error('Invalid operator');
+    }
+};
 
 module.exports = calc;
