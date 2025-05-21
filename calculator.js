c87805fa1d487e284af3d96a9a5bf3145ef25fe5
mohammadasaad1 <mohammadfoqha2@hotmail.com>
2025-05-22 01:26:12 +0300

    TEST : IMPLEMENT MULTIPLCATION  IN OUR FUNCTION

--- calculator.js
+++ calculator.js
@@ -5,6 +5,7 @@ const calc = (num1, operator, num2) => {
     switch(operator){
         case '+' : return num1 + num2 ;
         case '-' : return num1 - num2;
+        case '*': return a * b;
         break;
         default: throw new Error('Invalid operator');
     }
