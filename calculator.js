981d406f055b631c2264ffe781317ea7ab729f2c
mohammadasaad1 <mohammadfoqha2@hotmail.com>
2025-05-22 01:50:32 +0300

    TEST : handle the rest of the task

--- calculator.js
+++ calculator.js
@@ -1,19 +1,59 @@
-const calc = (num1, operator, num2) => {
-    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
-        throw new Error('Invalid input...'); 
+
+function calc(...args) {
+  
+  for (let i = 0; i < args.length; i += 2) {
+    const num = args[i];
+    if (typeof num !== 'number') {
+      throw new Error('Invalid input type');
     }
-    switch(operator){
-        case '+' : return num1 + num2 ;
-        case '-' : return num1 - num2;
-        case '*': return a * b;
-        case '/':
-        if (num2 === 0) {
-         throw new Error('Division by zero');
-        }
-         return num1 / num2;
-        // no need for break key-word :) 
-        default: throw new Error('Invalid operator');
+  }
+
+  
+  for (let i = 1; i < args.length; i += 2) {
+    const operator = args[i];
+    if (typeof operator !== 'string' || !['+', '-', '*', '/'].includes(operator)) {
+      throw new Error('Invalid operator');
     }
-};
+  }
+
+  // no numbers > 1000
+   args = args.map((val, i) => (i % 2 === 0 && val > 1000 ? 0 : val));
+
+  // [/][*]
+  let result = [args[0]];
+  let i = 1;
+
+  while (i < args.length) {
+    const operator = args[i];
+    const num2 = args[i + 1];
+    const lastNum = result[result.length - 1];
+
+    if (operator === '*') {
+      result[result.length - 1] = lastNum * num2;
+    } else if (operator === '/') {
+      if (num2 === 0) throw new Error('Division by zero');
+      result[result.length - 1] = lastNum / num2;
+    } else {
+      result.push(operator, num2);
+    }
+
+    i += 2;
+  }
+
+  //  [+][-]
+  let finalResult = result[0];
+  for (let j = 1; j < result.length; j += 2) {
+    const operator = result[j];
+    const num2 = result[j + 1];
+
+    if (operator === '+') {
+      finalResult += num2;
+    } else if (operator === '-') {
+      finalResult -= num2;
+    }
+  }
+
+  return finalResult;
+}
 
 module.exports = calc;
