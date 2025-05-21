4a650049529cb7157f392aecce47699fcf14b716
mohammadasaad1 <mohammadfoqha2@hotmail.com>
2025-05-22 01:24:17 +0300

    TEST : IMPLEMENT SUBTRACTION IN OUR FUNCTION

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ const calc = (num1, operator, num2) => {
     }
     switch(operator){
         case '+' : return num1 + num2 ;
+        case '-' : return num1 - num2;
         break;
         default: throw new Error('Invalid operator');
     }
